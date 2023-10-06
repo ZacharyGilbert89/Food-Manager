d074894b267e71d5f98b7d143026d7483b922697
Zach_Gilbert <99516776+ZacharyGilbert89@users.noreply.github.com>
2023-10-06 17:33:38 -0400

    Update deleteitem.cs

--- deleteitem.cs
+++ deleteitem.cs
@@ -15,33 +15,18 @@ namespace Food_
     {
         public deleteitem()
         {
-            InitializeComponent();
-
+            InitializeComponent(); //C# included line to make sure everything is updated
         }
-
-        private void label2_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void refreshBTN_Click(object sender, EventArgs e)
+        private void refreshBTN_Click(object sender, EventArgs e) //refreshes the items in case something was added
         {
             using (Form1 f1 = new Form1())
             {
-                TextReader reader = new StreamReader(f1.FP);
-
+                TextReader reader = new StreamReader(f1.FP); //reads the file and updates the list with any new changes
                 itemListTEXTBOX.Text = reader.ReadToEnd();
                 reader.Close();
-
             }
         }
-
-        private void deleteBTN_Click(object sender, EventArgs e)
+        private void deleteBTN_Click(object sender, EventArgs e) //function to delete specific items
         {
             using (Form1 f1 = new Form1())
             {
@@ -50,33 +35,34 @@ namespace Food_
                 FileStream inFile = new FileStream(path, FileMode.Open, FileAccess.Read);
                 StreamReader reader = new StreamReader(inFile);
                 string record;
-
-                string search = itemNameDelete.Text;
-
+                string search = itemNameDelete.Text; //opens the file
                 //---------------------------------------------------------------------------
-
                 record = reader.ReadLine();
                 if (search != null)
                 {
                     if (record.Contains(search))
                     {
-                        string[] output = record.Split(',');
-                        
+                        string[] output = record.Split(',');  //parses the found item
                     }
                     if (!record.Contains(search))
                     {
                         reader.Close();
                         inFile.Close();
-                        
                     }
                 }
             }
         }
-
         private void deleteitem_Load(object sender, EventArgs e)
         {
 
         }
-    }
+        private void label2_Click(object sender, EventArgs e)
+        {
 
+        }
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
 }
